93cfa9a0eb91132550989170eeef6bd57d76ba5a
nourakram1 <nourakram94@gmail.com>
2025-03-18 17:09:31 +0200

    Initializing the Simplex Solver class

---
+++ LP_Solver/flask-backend/SimplexSolver.py
@@ -0,0 +1,19 @@
+
+class SymplexSolver:
+    def __init__(self) -> None:
+        pass
+
+    def _init_tableau(self):
+        pass
+
+    def _find_entering_variable(self) -> int:
+        pass
+
+    def _find_leaving_variable(self) -> int:
+        pass
+
+    def _pivot(self) -> None:
+        pass
+
+    def solve(self) -> NotImplemented:
+        pass
